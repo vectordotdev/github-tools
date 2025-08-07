6eb81cb2491cbb24dfb94c765d9615d8ccdfdc3d
Pavlos Rontidis <pavlos.rontidis@gmail.com>
2025-08-07 10:08:02 -0400

    august 7th 2025 - updated

--- generate_all.sh
+++ generate_all.sh
@@ -37,6 +37,5 @@ for i in "${!INPUT_FILES[@]}"; do
 
   START_DATE=$(date -d "$(date +%Y-%m-01) -12 months" +%Y-%m)
   python scripts/util/plot.py --env-file "$env_file" --start "$START_DATE" --input-dir out/summaries \
-    --exclude-labels "no-changelog,meta: awaiting author
-
+    --exclude-labels "no-changelog,meta: awaiting author"
 done
